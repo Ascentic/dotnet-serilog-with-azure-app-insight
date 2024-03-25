f8a5a43dce9f846713bf0c6f778335e210306640
Shazni Shiraz <shaznishiraz+github@gmail.com>
2024-03-25 22:45:44 +0530

    Create API.Serilog.AppInsights.csproj

---
+++ src/API.Serilog.AppInsights/Common/ThreadIdEnricher.cs
@@ -0,0 +1,17 @@
+﻿// <copyright file="ThreadIdEnricher.cs" company="Ascentic">
+// Copyright (c) Ascentic. All rights reserved.
+// </copyright>
+
+namespace API.Common;
+
+using Serilog.Core;
+using Serilog.Events;
+
+public class ThreadIdEnricher : ILogEventEnricher
+{
+	public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
+	{
+		logEvent.AddPropertyIfAbsent(propertyFactory.CreateProperty(
+				"ThreadId", Thread.CurrentThread.ManagedThreadId));
+	}
+}
