9aa63724224dd1381c77d3ed9a0e35453ab38e87
Shazni Shiraz <shaznishiraz+github@gmail.com>
2024-03-25 22:47:22 +0530

    Create AppSettingsConstants.cs

---
+++ src/API.Serilog.AppInsights/Constants/AppSettingsConstants.cs
@@ -0,0 +1,14 @@
+﻿// <copyright file="AppSettingsConstants.cs" company="Ascentic">
+// Copyright (c) Ascentic. All rights reserved.
+// </copyright>
+
+namespace API.Constants;
+
+public class AppSettingsConstants
+{
+	public const string AppInsightInstrumentationKeyForWebSites = "APPINSIGHTS_INSTRUMENTATIONKEY";
+	public const string AppInsightConnectionStringEnvironmentVariable = "APPLICATIONINSIGHTS_CONNECTION_STRING";
+	public const string LogLevel = nameof(LogLevel);
+	public const string Default = nameof(Default);
+	public const string IsUseDefaultLogger = nameof(IsUseDefaultLogger);
+}
