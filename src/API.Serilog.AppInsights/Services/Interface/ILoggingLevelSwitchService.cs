3861d53f91a39021ad7537d26ee9df9dce3f52b2
Shazni Shiraz <shaznishiraz+github@gmail.com>
2024-03-25 22:47:17 +0530

    Create LoggingLevelSwitchService

---
+++ src/API.Serilog.AppInsights/Services/Interface/ILoggingLevelSwitchService.cs
@@ -0,0 +1,15 @@
+﻿// <copyright file="ILoggingLevelSwitchService.cs" company="Ascentic">
+// Copyright (c) Ascentic. All rights reserved.
+// </copyright>
+
+namespace API.Services.Interface;
+
+using Serilog.Core;
+using Serilog.Events;
+
+public interface ILoggingLevelSwitchService
+{
+	LoggingLevelSwitch LevelSwitch { get; }
+
+	void SetLogLevel(LogEventLevel level);
+}
---
+++ src/API.Serilog.AppInsights/Services/LoggingLevelSwitchService.cs
@@ -0,0 +1,21 @@
+﻿// <copyright file="LoggingLevelSwitchService.cs" company="Ascentic">
+// Copyright (c) Ascentic. All rights reserved.
+// </copyright>
+
+namespace API.Services;
+
+using API.Services.Interface;
+using Serilog.Core;
+using Serilog.Events;
+
+public class LoggingLevelSwitchService : ILoggingLevelSwitchService
+{
+	private readonly LoggingLevelSwitch levelSwitch = new LoggingLevelSwitch();
+
+	public LoggingLevelSwitch LevelSwitch => this.levelSwitch;
+
+	public void SetLogLevel(LogEventLevel level)
+	{
+		this.levelSwitch.MinimumLevel = level;
+	}
+}
