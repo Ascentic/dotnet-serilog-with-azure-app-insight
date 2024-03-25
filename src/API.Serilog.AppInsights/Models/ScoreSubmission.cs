04c14a3f7686434ce87ab5be41ff5cbe14c1c00b
Shazni Shiraz <shaznishiraz+github@gmail.com>
2024-03-25 22:47:27 +0530

    Create ScoreSubmission.cs

---
+++ src/API.Serilog.AppInsights/Models/ScoreSubmission.cs
@@ -0,0 +1,21 @@
+﻿// <copyright file="ScoreSubmission.cs" company="Ascentic">
+// Copyright (c) Ascentic. All rights reserved.
+// </copyright>
+
+namespace API.Models;
+
+/// <summary>
+/// Represents a score submission by a player.
+/// </summary>
+public class ScoreSubmission
+{
+	/// <summary>
+	/// Gets or sets the name of the player submitting the score.
+	/// </summary>
+	required public string PlayerName { get; set; }
+
+	/// <summary>
+	/// Gets or sets the score submitted by the player.
+	/// </summary>
+	public int Score { get; set; }
+}
